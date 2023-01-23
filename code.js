5e1d2b1121f5c892731f9176f5c72f11358aa7b7
Alexander <alexanderguest@me.com>
2023-01-23 12:57:45 -0500

    newest

--- code.js
+++ code.js
@@ -1,28 +1,46 @@
 // submit 6 Pokemon Characters if each character value is True 
 
-let teamPokemonNumber = 3 //0 by default
-const teamList = document.querySelector("#team-list")
+// let teamPokemonNumber = 1 //0 by default
+// const teamList = document.querySelector("#team-list")
 
-test()
-function test(){
-    let num = 0
-    let stop = 0
-    while (num < teamPokemonNumber){
-        fetch('https://example.com/profile')
-        .then((response) => response.json())
-        .then((data) => { //pokemon name url resolves
-            num++
-            /*
-            logic for making html pokemon
-            */
-            console.log('Success:', data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        })
-        stop++
-        if (stop == 10){
-            num = teamPokemonNumber
-        }
-    }
-}
+// test()
+// function test() {
+
+//     let pokeName = "Pikachu"
+//     while (num < teamPokemonNumber) {
+//         fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName.toLowerCase()}`)
+//             .then(response => response.json())
+//             .then(data => {
+//                 num++
+//                 console.log(data)
+//                 /*
+//                 logic for making html pokemon
+//                 */
+//                 console.log('Success:', data);
+//             })
+//             .catch((error) => {
+//                 console.error('Error:', error);
+//             })
+//         stop++
+//         if (stop == 10) {
+//             num = teamPokemonNumber
+//         }
+//     }
+// }
+
+const pokeForm = document.querySelector("#pokeform")
+const pokeRoster = document.querySelector("#team-list")
+pokeForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const pokeDiv = document.createElement('div')
+    pokeDiv.setAttribute('id', 'pokeNumber')
+    pokeDiv.setAttribute('class', 'pokemonObj')
+    const pokeName = document.createElement('p')
+    pokeName.textContent = pokemon.name
+
+
+
+
+    pokeRoster.append(pokeDiv)
+
+});
