8c73cf2928193a06b7329e6921ddcb3daf15b092
+ <stingeyandrewstown@live.com>
2023-01-23 10:10:39 -0600

    yes

--- code.js
+++ code.js
@@ -1,2 +1,29 @@
 // submit 6 Pokemon Characters if each character value is True 
 
+let teamPokemonNumber = 3 //0 by default
+const teamList = document.querySelector("#team-list")
+
+test()
+function test(){
+    let num = 0
+    while (num < teamPokemonNumber){
+        fetch('https://example.com/profile', {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then((response) => response.json())
+        .then((data) => { //pokemon name url resolves
+            num++
+            /*
+            logic for making html pokemon
+            */
+            console.log('Success:', data);
+        })
+        .catch((error) => { //pokemon name url fails
+            console.error('Error:', error);
+        });
+    }
+}
